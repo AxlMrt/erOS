d00bd63e97ff815a1cb30df85fe414e15f570d0b
Axel Martin <martin.axel@sfr.fr>
2026-02-17 16:32:03 +0100

    [Security] Add basic pentesting and cybersecurity tools

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -5,9 +5,14 @@
     ./hardware-configuration.nix
     ../../modules/core/system.nix
     ../../modules/desktop/hyprland.nix
+    ../../modules/security/base-tools.nix
+    ../../modules/security/recon.nix
+    ../../modules/security/web.nix
   ];
 
   networking.hostName = "axlmrt-laptop";
+
+  nixpkgs.config.allowUnfree = true;
 
   time.timeZone = "Europe/Paris";
 
