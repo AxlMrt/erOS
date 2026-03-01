f52fd6ff4060f249099358a990375b5182da03db
Axel Martin <martin.axel@sfr.fr>
2026-03-01 13:48:15 +0100

    chore(browsers/firefox): add Bitwarden addon

--- modules/home-manager/browers/firefox.nix
+++ modules/home-manager/browers/firefox.nix
@@ -169,6 +169,11 @@ in {
       SearchBar = "unified"; # alternative: "separate"
       ExtensionSettings = {
         "*".installation_mode = "blocked";
+        # Bitwarden
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+        };
         # uBlock Origin:
         "uBlock0@raymondhill.net" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
